b0df76ba8dc4cc47b45785ada7fb82d3d537ce35
Fabio <fmdefranca@gmail.com>
2020-08-04 17:33:03 +0100

    created the menu object

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -26,70 +26,347 @@ const useStyles = makeStyles((theme) => ({
 
 const cards = [
   {
-    name: "fish",
+    name: "Fish",
+    items: [
+      {
+        name: "Haddock",
+        large: 7.15,
+        medium: 6.25,
+        regular: null,
+      },
+      {
+        name: "Cod",
+        large: 7.25,
+        medium: 6.35,
+        regular: 5.4,
+      },
+      {
+        name: "Senior's Fish & Chips",
+        large: null,
+        medium: null,
+        regular: 4.2,
+      },
+      {
+        name: "Plaice",
+        large: null,
+        medium: null,
+        regular: 6.0,
+      },
+      {
+        name: "Scampi",
+        large: null,
+        medium: null,
+        regular: 5.95,
+      },
+      {
+        name: "Salt & Pepper Squid",
+        large: 4.95,
+        medium: null,
+        regular: null,
+      },
+      {
+        name: "Fishcake",
+        large: 1.95,
+        medium: null,
+        regular: null,
+      },
+      {
+        name: "Cod Roe",
+        large: 1.95,
+        medium: null,
+        regular: null,
+      },
+      {
+        name: "Chips",
+        large: 3.2,
+        medium: null,
+        regular: 2.4,
+      },
+    ],
+  },
+  {
+    name: "Ribs",
+    items: [
+      {
+        name: "Rack of Ribs",
+        large: 6.95,
+        medium: 6.45,
+        regular: null,
+      },
+      {
+        name: "Mega Mixer",
+        large: null,
+        medium: 9.5,
+        regular: null,
+      },
+      {
+        name: "1 Medium Rack of Ribs & 2 pieces of Southern Fried Chicken",
+        large: null,
+        medium: 5.5,
+        regular: null,
+      },
+      {
+        name: "1/4 Spit Roast Chicken Mixer",
+        large: null,
+        medium: 5.5,
+        regular: null,
+      },
+      {
+        name: "1/2 Rack of Ribs & 1 Piece of Southern Fried Chicken",
+        large: null,
+        medium: null,
+        regular: null,
+      },
+    ],
+  },
+  {
+    name: "Chicken",
     items: [
       {
         name: "Chicken & Mushroom",
-        low: null,
-        med: null,
-        high: 2.95,
-      },
-      {
-        name: "Beef & Kidney",
-        low: null,
-        med: null,
-        high: 2.95,
+        large: null,
+        medium: null,
+        regular: 2.95,
       },
     ],
   },
   {
-    name: "chicken",
-    items: [
-      {
-        name: "Steak & Kidney",
-        low: null,
-        med: null,
-        high: 2.95,
-      },
-      {
-        name: "Jumbo Sausage",
-        low: null,
-        med: null,
-        high: 1.95,
-      },
-    ],
-  },
-  {
-    name: "beef",
-    items: [
-      {
-        name: "Battered Sausage",
-        low: null,
-        med: null,
-        high: 1.95,
-      },
-      {
-        name: "Savaloy",
-        low: null,
-        med: null,
-        high: 1.95,
-      },
-    ],
-  },
-  {
-    name: "ribs",
+    name: "Burgers",
     items: [
       {
         name: "6oz Gourmet Burger",
-        low: null,
-        med: null,
-        high: 4.0,
+        large: null,
+        medium: null,
+        regular: 4.0,
       },
       {
         name: "6oz Gourmet Cheese Burger",
-        low: null,
-        med: null,
-        high: 4.3,
+        large: null,
+        medium: null,
+        regular: 4.3,
+      },
+      {
+        name: "1/4lb Beef Burger",
+        large: null,
+        medium: null,
+        regular: 3.2,
+      },
+      {
+        name: "1/4 Beef Burger with Cheese",
+        large: null,
+        medium: null,
+        regular: 3.4,
+      },
+      {
+        name: "Chicken Burger",
+        large: null,
+        medium: null,
+        regular: 3.4,
+      },
+      {
+        name: "Royal Chicken Burger",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Chicken Burger with onion rings and cheese",
+        large: null,
+        medium: null,
+        regular: null,
+      },
+      {
+        name: "Veggie Burger",
+        large: null,
+        medium: null,
+        regular: 2.9,
+      },
+    ],
+  },
+  {
+    name: "Pies",
+    items: [
+      {
+        name: "Beef & Kidney",
+        large: null,
+        medium: null,
+        regular: 2.95,
+      },
+      {
+        name: "Steak & Kidney",
+        large: null,
+        medium: null,
+        regular: 2.95,
+      },
+    ],
+  },
+  {
+    name: "Kids",
+    items: [
+      {
+        name: "Chicken Nuggets Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Southern Fried Chicken Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Sausage Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+      {
+        name: "Saveloy Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+      {
+        name: "Cod Fillet Strip Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Fishcake Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+    ],
+  },
+  {
+    name: "Sides",
+    items: [
+      {
+        name: "Onion Rings",
+        large: null,
+        medium: null,
+        regular: 1.8,
+      },
+      {
+        name: "Breaded Mushrooms",
+        large: null,
+        medium: null,
+        regular: 1.8,
+      },
+      {
+        name: "Bread Roll",
+        large: null,
+        medium: null,
+        regular: 0.75,
+      },
+      {
+        name: "Chip Roll",
+        large: null,
+        medium: null,
+        regular: 2.4,
+      },
+      {
+        name: "Curry Sauce",
+        large: null,
+        medium: null,
+        regular: 1.7,
+      },
+      {
+        name: "Mushy Peas",
+        large: null,
+        medium: null,
+        regular: 1.7,
+      },
+      {
+        name: "Pickled Onions",
+        large: null,
+        medium: null,
+        regular: 0.55,
+      },
+      {
+        name: "Pickled Egg",
+        large: null,
+        medium: null,
+        regular: 0.7,
+      },
+      {
+        name: "Gherkin",
+        large: null,
+        medium: null,
+        regular: 0.7,
+      },
+      {
+        name: "Tartare Sauce",
+        large: null,
+        medium: null,
+        regular: 1.6,
+      },
+      {
+        name: "Bottles of Sauce / Vinegar",
+        large: null,
+        medium: null,
+        regular: 1.6,
+      },
+      {
+        name: "Sauce Dips",
+        large: null,
+        medium: null,
+        regular: 0.35,
+      },
+      {
+        name: "Pineapple Fritter",
+        large: null,
+        medium: null,
+        regular: 1.4,
+      },
+    ],
+  },
+  {
+    name: "Sausages",
+    items: [
+      {
+        name: "Jumbo Sausage",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+      {
+        name: "Battered Sausage",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+      {
+        name: "Savaloy",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+    ],
+  },
+  {
+    name: "Drinks",
+    items: [
+      {
+        name: "Cold Cans",
+        large: null,
+        medium: null,
+        regular: 1.0,
+      },
+      {
+        name: "Fruit Shoots",
+        large: null,
+        medium: null,
+        regular: 0.95,
+      },
+      {
+        name: "Star Slush",
+        large: 1.35,
+        medium: 1.1,
+        regular: null,
       },
     ],
   },
@@ -117,7 +394,9 @@ export default function Album() {
                   </Typography>
                   {card.items.map((n) => (
                     <Typography>
-                      <span>{n.name} : {n.low} {n.med} {n.high}</span>
+                      <span>
+                        {n.name} : {n.large} {n.medium} {n.regular}
+                      </span>
                     </Typography>
                   ))}
                 </CardContent>
--- src/pages/menuItems.js
+++ src/pages/menuItems.js
@@ -1,48 +1,292 @@
- {
-	'Chicken & Mushroom',
-	null,		 
-	null,		 
-	2.95,		 
-},
- {
-	'Beef & Kidney',
-	null,		 
-	null,		 
-	2.95,		 
-},
- {
-	'Steak & Kidney',
-	null,		 
-	null,		 
-	2.95,		 
-},
- {
-	'Jumbo Sausage',
-	null,		 
-	null,		 
-	1.95,		 
-},
- {
-	'Battered Sausage',
-	null,		 
-	null,		 
-	1.95,		 
-},
- {
-	'Savaloy',
-	null,		 
-	null,		 
-	1.95,		 
-},
- {
-	'6oz Gourmet Burger',
-	null,		 
-	null,		 
-	4.00,		 
-},
- {
-	'6oz Gourmet Cheese Burger',
-	null,		 
-	null,		 
-	4.30,		 
-},
+const cards = [
+  {
+    name: "Fish",
+    items: [],
+  },
+  {
+    name: "Ribs",
+    items: [
+      {
+        name: "Rack of Ribs",
+        large: 6.95,
+        medium: 6.45,
+        regular: null,
+      },
+      {
+        name: "Mega Mixer",
+        large: null,
+        medium: 9.5,
+        regular: null,
+      },
+      {
+        name: "1 Medium Rack of Ribs & 2 pieces of Southern Fried Chicken",
+        large: null,
+        medium: 5.5,
+        regular: null,
+      },
+      {
+        name: "1/4 Spit Roast Chicken Mixer",
+        large: null,
+        medium: 5.5,
+        regular: null,
+      },
+      {
+        name: "1/2 Rack of Ribs & 1 Piece of Southern Fried Chicken",
+        large: null,
+        medium: null,
+        regular: null,
+      },
+    ],
+  },
+  {
+    name: "Chicken",
+    items: [
+      {
+        name: "Chicken & Mushroom",
+        large: null,
+        medium: null,
+        regular: 2.95,
+      },
+    ],
+  },
+  {
+    name: "Burgers",
+    items: [
+      {
+        name: "6oz Gourmet Burger",
+        large: null,
+        medium: null,
+        regular: 4.0,
+      },
+      {
+        name: "6oz Gourmet Cheese Burger",
+        large: null,
+        medium: null,
+        regular: 4.3,
+      },
+      {
+        name: "1/4lb Beef Burger",
+        large: null,
+        medium: null,
+        regular: 3.2,
+      },
+      {
+        name: "1/4 Beef Burger with Cheese",
+        large: null,
+        medium: null,
+        regular: 3.4,
+      },
+      {
+        name: "Chicken Burger",
+        large: null,
+        medium: null,
+        regular: 3.4,
+      },
+      {
+        name: "Royal Chicken Burger",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Chicken Burger with onion rings and cheese",
+        large: null,
+        medium: null,
+        regular: null,
+      },
+      {
+        name: "Veggie Burger",
+        large: null,
+        medium: null,
+        regular: 2.9,
+      },
+    ],
+  },
+  {
+    name: "Pies",
+    items: [
+      {
+        name: "Beef & Kidney",
+        large: null,
+        medium: null,
+        regular: 2.95,
+      },
+      {
+        name: "Steak & Kidney",
+        large: null,
+        medium: null,
+        regular: 2.95,
+      },
+    ],
+  },
+  {
+    name: "Kids",
+    items: [
+      {
+        name: "Chicken Nuggets Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Southern Fried Chicken Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Sausage Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+      {
+        name: "Saveloy Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+      {
+        name: "Cod Fillet Strip Meal",
+        large: null,
+        medium: null,
+        regular: 3.85,
+      },
+      {
+        name: "Fishcake Meal",
+        large: null,
+        medium: null,
+        regular: 3.75,
+      },
+    ],
+  },
+  {
+    name: "Sides",
+    items: [
+      {
+        name: "Onion Rings",
+        large: null,
+        medium: null,
+        regular: 1.8,
+      },
+      {
+        name: "Breaded Mushrooms",
+        large: null,
+        medium: null,
+        regular: 1.8,
+      },
+      {
+        name: "Bread Roll",
+        large: null,
+        medium: null,
+        regular: 0.75,
+      },
+      {
+        name: "Chip Roll",
+        large: null,
+        medium: null,
+        regular: 2.4,
+      },
+      {
+        name: "Curry Sauce",
+        large: null,
+        medium: null,
+        regular: 1.7,
+      },
+      {
+        name: "Mushy Peas",
+        large: null,
+        medium: null,
+        regular: 1.7,
+      },
+      {
+        name: "Pickled Onions",
+        large: null,
+        medium: null,
+        regular: 0.55,
+      },
+      {
+        name: "Pickled Egg",
+        large: null,
+        medium: null,
+        regular: 0.7,
+      },
+      {
+        name: "Gherkin",
+        large: null,
+        medium: null,
+        regular: 0.7,
+      },
+      {
+        name: "Tartare Sauce",
+        large: null,
+        medium: null,
+        regular: 1.6,
+      },
+      {
+        name: "Bottles of Sauce / Vinegar",
+        large: null,
+        medium: null,
+        regular: 1.6,
+      },
+      {
+        name: "Sauce Dips",
+        large: null,
+        medium: null,
+        regular: 0.35,
+      },
+      {
+        name: "Pineapple Fritter",
+        large: null,
+        medium: null,
+        regular: 1.4,
+      },
+    ],
+  },
+  {
+    name: "Sausages",
+    items: [
+      {
+        name: "Jumbo Sausage",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+      {
+        name: "Battered Sausage",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+      {
+        name: "Savaloy",
+        large: null,
+        medium: null,
+        regular: 1.95,
+      },
+    ],
+  },
+  {
+    name: "Drinks",
+    items: [
+      {
+        name: "Cold Cans",
+        large: null,
+        medium: null,
+        regular: 1.0,
+      },
+      {
+        name: "Fruit Shoots",
+        large: null,
+        medium: null,
+        regular: 0.95,
+      },
+      {
+        name: "Star Slush",
+        large: 1.35,
+        medium: 1.1,
+        regular: null,
+      },
+    ],
+  },
+];
